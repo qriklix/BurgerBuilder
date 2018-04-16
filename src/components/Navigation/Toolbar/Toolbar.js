d3541633ab7aa5ee5fd1e4c6180768dd4e6814e7
Qriklix <qriklix@zergling.hu>
2018-04-16 23:03:58 +0200

    Add: sidedrawer

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -7,8 +7,12 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
         <div>Menu</div>
-        <Logo />
-        <NavigationItems />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav>
+            <NavigationItems />
+        </nav>
     </header>
 );
 
